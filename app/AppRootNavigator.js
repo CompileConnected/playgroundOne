c8bd84218bdcc46c7d0f438044b2e8f5aba74077
yohanes <yokho95@gmail.com>
2018-09-22 23:14:12 +0700

    first commit

---
+++ app/AppRootNavigator.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { Dimensions, Platform } from 'react-native';
+import { createBottomTabNavigator, StackNavigator } from 'react-navigation';
+import { Icon } from 'react-native-elements';
+
+
+import { Explore } from './screens/Explore';
+import { AddBook } from './screens/AddBook';
+import { Lists as ReadingListStack } from './screens/Lists';
+import { Profile } from './screens/Profile';
+import { BookCase } from './screens/BookCase';
+
+let screen = Dimensions.get('window');
+
+let tabsIconSize = 24;
+
+export const Tabs = createBottomTabNavigator({
+    'Book': {
+      screen: BookCase,
+      navigationOptions: {
+        tabBarLabel: 'Bookcase',
+        tabBarIcon: ({ tintColor }) => <Icon name="book" type="materialIcons" size={tabsIconSize} color={tintColor} />
+      },
+    },
+    'Explore': {
+      screen: Explore,
+      navigationOptions: {
+        tabBarLabel: 'Explore',
+        tabBarIcon: ({ tintColor }) => <Icon name="explore" type="materialIcons" size={tabsIconSize} color={tintColor} />
+      },
+    },
+    'Add Book': {
+      screen: AddBook,
+      navigationOptions: {
+        tabBarLabel: 'Add Book',
+        tabBarIcon: ({ tintColor }) => <Icon name="add" type="materialIcons" size={tabsIconSize} color={tintColor} />
+      },
+    },
+    'Lists': {
+      screen: ReadingListStack,
+      navigationOptions: {
+        tabBarLabel: 'Lists',
+        tabBarIcon: ({ tintColor }) => <Icon name="list" type="entypo" size={tabsIconSize} color={tintColor} />
+      },
+    },
+    'My Profile': {
+      screen: Profile,
+      navigationOptions: {
+        tabBarLabel: 'Profile',
+        tabBarIcon: ({ tintColor }) => <Icon name="person" type="materialIcons" size={tabsIconSize} color={tintColor} />
+      },
+    },
+  });
+  
+  export const createRootNavigator = () => {
+    return StackNavigator(
+      {
+        Tabs: {
+          screen: Tabs,
+          navigationOptions: {
+            gesturesEnabled: false
+          }
+        }
+      },
+      {
+        headerMode: "none",
+        mode: "modal"
+    }
+    );
+  };
---
+++ app/screens/Explore.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export class Explore extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          Explore
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  }
+});
+
+export default Explore
